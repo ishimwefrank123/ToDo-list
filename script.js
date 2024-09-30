5b7de930b5cf7f0845d6fb28d34dcf4d1c0513ff
ishimwefrank123 <ishimwefrank147@gmail.com>
2024-09-30 02:27:02 +0200

    the checked task and unchecked the task and delete button functionality

--- script.js
+++ script.js
@@ -14,4 +14,13 @@
         li.appendChild(span);
     }
     inputBox.value = "";
- }  
+ }   
+
+ listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+ }, false)
